d8ade821f904d933f7e249499782cdbd35785e76
minuro <kotoribuki@gmail.com>
2020-01-04 18:41:33 +0900

    [add] Top,Roomの追加

---
+++ src/views/Room.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container text-center>
+    <v-layout row wrap justify-center>
+      <v-flex xs12>
+        <h1>部屋</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
---
+++ src/views/Top.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container text-center>
+    <v-layout row wrap justify-center>
+      <v-flex xs12>
+        <h1>部屋番号を入力してください</h1>  
+        <router-link :to="{ name: 'room' }">
+          <v-btn color="info" min-width="80px">
+            入室する
+          </v-btn>
+        </router-link>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<style lang="scss">
+a {
+  text-decoration: none;
+}
+</style>
