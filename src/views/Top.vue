18ae28cbde9db8cc0650a686f3b87f143d257a88
minuro <kotoribuki@gmail.com>
2020-01-04 23:26:13 +0900

    [update] topの入力フォーム作成

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -2,19 +2,72 @@
   <v-container text-center>
     <v-layout row wrap justify-center>
       <v-flex xs12>
+        <v-alert
+        border="left"
+        colored-border
+        type="error"
+        elevation="2"
+        v-if="errorMessage"
+        >
+          {{errorMessage}}
+        </v-alert>
+      </v-flex>
+      <v-flex xs12>
         <h1>部屋番号を入力してください</h1>  
-        <router-link :to="{ name: 'room' }">
-          <v-btn color="info" min-width="80px">
-            入室する
-          </v-btn>
-        </router-link>
+      </v-flex>
+      <v-flex xs6 sm4 md2>
+        <v-text-field 
+        class="text-field"
+        maxlength="4" 
+        solo 
+        single-line
+        v-model="roomNo"
+        />
+        <v-btn color="info" min-width="80px" @click="joinRoom">
+          入室する
+        </v-btn>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
+<script>
+export default {
+  data() {
+    return{
+      errorMessage: '',
+      roomNo: ''
+    }
+  },
+  methods: {
+    joinRoom(){
+      this.errorMessage = this.checkInput(this.roomNo)
+      if(!this.errorMessage){
+        this.$router.push({name: 'room'})
+      }
+    },
+    checkInput(num){
+      if(!num){
+        return '部屋番号を入力してください'
+      }
+      if(num.length != 4){
+        return '4文字で入力してください'
+      }
+      if(/\D/g.test(num)){
+        return '数字で入力してください'
+      }
+      
+      return ''
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 a {
   text-decoration: none;
+}
+.text-field input{
+  text-align: center;
 }
 </style>
